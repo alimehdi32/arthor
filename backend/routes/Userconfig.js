4179055e70d0d40f0ca77a9be9c97d46791daf06
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-07-02 15:35:26 +0530

    login route configured

--- backend/routes/Userconfig.js
+++ backend/routes/Userconfig.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const signUp = require('../user/signup');
+const { login } = require('../user/login');
 
 router.post('/signup', signUp.signUp);
+router.post('/login', login);
 
 module.exports = router;
---
+++ backend/user/login.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user');
+
+exports.login = async (req, res) => {
+
+    const { email, password } = req.body;
+     console.log('-------------Login request received------------------');
+    try{
+
+        // check if user exists
+        const user = await userModel.findOne({ email });
+        if( !user ) {
+             return res.status(500).json({
+                success: false,
+                message: 'User does not exist',
+             });
+        }
+        console.log('-------------User found------------------');
+
+        const isMatched = await bcrypt.compare(password, user.password);
+        if( !isMatched ){
+            console.log('-------------Password mismatch------------------');
+            return res.status(500).json({
+                success: false,
+                message: 'Invalid credentials',
+            })
+        }
+        console.log('-------------Password matched------------------');
+
+        // generate JWT token
+        const token = jwt.sign(
+            { id: user._id, username: user.username, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' },
+        );
+        console.log('-------------JWT token generated:------------------', token);
+
+        // set cookie with JWT token
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // set to true in production
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        })
+        console.log('-------------Cookie set with JWT token------------------');
+
+
+        return res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+            error: error.message,
+        });
+
+    }
+}
