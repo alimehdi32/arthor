158d7898cdc84602755536f4e0be202e0c7dda17
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-07-08 15:06:42 +0530

    frontend started

---
+++ frontend/app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between h-20 w-full border border-b-gray-800 border-black p-4 text-white'>
+      OZXORA
+    </div>
+  )
+}
+
+export default Navbar
