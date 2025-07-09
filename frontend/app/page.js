714b3397c082f8b7f4d99c6a22d13947df25fd1d
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-07-09 23:54:26 +0530

    working on default page UI

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,8 +3,15 @@
 export default function Home() {
   return (
     <div>
-      
-      <h1>Welcome to ozxora</h1>
+
+      <div className='flex items-center justify-between w-full border border-b-gray-800 border-black p-4 text-white'>
+        <div className="pl-4">
+          <p className="ml-0 ">WelCome to Bravin</p>
+          <p className="ml-0 ">Your intelligent guide to mastering any skill</p>
+          <p className="ml-0 ">Stuck on what to learn next? We got you.</p>
+          <p className="ml-0 ">AIVID helps you figure out what to learn and how to learn it — with AI-generated roadmaps made just for you. Not ready to dive in? No worries. Videos unlock only when you are. No pressure, just progress. Let’s make learning feel less overwhelming and way more doable.</p>
+        </div>
+      </div>
     </div>
   );
 }
