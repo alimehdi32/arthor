2aaa6075b2ec25ed20a3dbe301162f504124b5ff
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-07-11 17:08:25 +0530

    features component completed

--- frontend/app/components/Card.js
+++ frontend/app/components/Card.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({index, feature, desc}) => {
+  console.log(index, feature, desc)
   return (
-    <div>
-      
-    </div>
+    <>
+      { (index <= 2) ?
+        <div className='bg-white/10 backdrop-blur-md max-w-[420px] h-[200px] hover:scale-105 hover:z-10 border border-white/20 p-4 rounded-lg shadow-md flex flex-col items-center justify-around m-2'>
+          <h2 className='text-lg font-semibold text-white'>{feature}</h2>
+          <p className='text-sm text-gray-300 mt-2'>{desc}</p>
+        </div> :
+        <div className='bg-white/10 backdrop-blur-md max-w-[420px] border h-[200px] hover:scale-105 hover:z-10 border-white/20 p-4 rounded-lg shadow-md row-start-2 flex flex-col items-center justify-around m-2'>
+          <h2 className='text-lg font-semibold text-white'>{feature}</h2>
+          <p className='text-sm text-gray-300 mt-2'>{desc}</p>
+        </div>}
+    </>
   )
 }
 
--- frontend/app/components/Features.js
+++ frontend/app/components/Features.js
@@ -1,7 +1,13 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState, useCallback } from 'react'
 import Card from './Card'
+import { useRouter } from "next/navigation";
 
 const Features = () => {
+    const Router = useRouter();
+    const [click, setClick] = useState(false);
+
+
     const features = [{
         feature: 'AI-Powered Roadmap Generator',
         desc: 'Get a full learning path tailored to your input. Just type your goal, and BRAVIN builds a roadmap from scratch.'
@@ -26,16 +32,29 @@ const Features = () => {
         feature: 'No Ads, No Distractions',
         desc: 'Enjoy an uninterrupted, clean learning experience without any distractions.'
     }]
+
+    const routeToFeatures = useCallback(async () => {
+        Router.push('/features') 
+    }, [Router]);
+
+    useEffect(() => {
+        if (click) {
+            routeToFeatures();
+        }
+    }, [click, routeToFeatures]);
     return (
-        <div className='h-[200px] bg-white/5 backdrop-blur-md border border-white/10 flex flex-col items-center justify-center text-center p-4  shadow-lg'>
-            <h1 className='font-semibold text-2xl from-neutral-800 font-sans'>Brains Meet AI — What makes Bravin Easy&Fun</h1>
+        <div className='h-[600px] bg-white/5 backdrop-blur-md border border-white/10 flex flex-col items-center justify-around text-center p-4  shadow-lg'>
+            <h1 className='font-semibold text-3xl from-neutral-800 font-sans'>Brains Meet AI — What makes Bravin Easy&Fun</h1>
             <div className='grid grid-rows-2 p-0'>
                 { features.map((item, index) => {
                     return (
-                        <Card key={index} feature={item.feature} desc={item.desc} />
+                        <Card key={index} index={index} feature={item.feature} desc={item.desc} />
                     )
                 })}
             </div>
+            <button onClick={() => {setClick(!click)}} className='bg-white/10 backdrop-blur-md border border-white/20 p-2 rounded-lg shadow-md hover:bg-white/20 transition-colors duration-300 text-white'>
+                Explore more features
+            </button>
         </div>
     )
 }
